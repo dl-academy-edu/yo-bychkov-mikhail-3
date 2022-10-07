35f75ff6abf891ac6715da7a00813d970b26d807
lenivec161 <cevinel161@mail.ru>
2022-10-07 23:46:02 +0600

    test fork

---
+++ index.js
@@ -0,0 +1,7 @@
+const myName = "Михаил";
+const programmingLanguage = "Javascript";
+const numberOfMonth = 5;
+
+const resultString = `Всем привет! Меня зовут, ${myName}. Я учу язык ${programmingLanguage}.
+Учу его уже ${numberOfMonth} месяцев.`;
+console.log(resultString);
